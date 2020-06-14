6065f555c395956b818abf243a07d708463ad98f
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-14 23:32:22 +0400

    comments

--- OOP/Classes.py
+++ OOP/Classes.py
@@ -1,25 +1,26 @@
-                                                                    Classes
+"""                                                                    Classes
                                                                     
 Objects are created using classes, which are actually the focal point of OOP.
 The class describes what the object will be, but is separate from the object itself. In other words, 
  a class can be described as an object's blueprint, description, or definition.
 You can use the same class as a blueprint for creating multiple different objects.
 Classes are created using the keyword class and an indented block, which contains class methods (which are functions). 
-                                                                    
+"""                                                                 
 class Cat:
   def __init__(self, color, legs):
     self.color = color
     self.legs = legs
 felix = Cat("ginger", 4)
 rover = Cat("dog-colored", 4)
-stumpy = Cat("brown", 3)                                                                    
+stumpy = Cat("brown", 3)   
 
+"""
 This code defines a class named Cat, which has two attributes: color and legs.
 Then the class is used to create 3 separate objects of that class.
-
+"""
 
 #1  __init__
-
+"""
 The __init__ method is the most important method in a class.
 This is called when an instance (object) of the class is created, using the class name as a function.
 
@@ -29,6 +30,7 @@ All methods must have self as their first parameter, although it isn't explicitl
 Instances of a class have attributes, which are pieces of data associated with them.
 In this example, Cat instances have attributes color and legs. These can be accessed by putting a dot, and the attribute name after an instance.
 In an __init__ method, self.attribute can therefore be used to set the initial value of an instance's attributes.
+"""
 
 class Cat:
     def __init__(self, color, legs):
@@ -37,8 +39,10 @@ class Cat:
 felix = Cat("ginger", 4)
 print(felix.color)                            #  ginger
 
+"""
 In the example above, the __init__ method takes two arguments and assigns them to the object's attributes. 
 The __init__ method is called the class constructor.
+"""
 
 class Student:
     def __init__(self, name):
@@ -74,9 +78,11 @@ john.talk()                                   # Hi, I'm John Smith
 
 #1.3
 
+"""
 Classes can have other methods defined to add functionality to them.
 Remember, that all methods must have self as their first parameter.
 These methods are accessed using the same dot syntax as attributes.
+"""
 
 class Person:
     def __init__(self, name, age):
@@ -92,8 +98,10 @@ john.talk()                                                       # Hi, I'm John
 
 #2
 
+"""
 Classes can also have class attributes, created by assigning variables within the body of the class. 
 These can be accessed either from instances of the class, or the class itself.
+"""
 
 class Person:
     hair_color = "black"
@@ -107,7 +115,9 @@ print(me.hair_color)                                              # black
 john = Person("John Smith", 25)
 print(john.hair_color)                                            # black
 
+"""
 Class attributes are shared by all instances of the class.
+"""
 
 #3  Error
 
