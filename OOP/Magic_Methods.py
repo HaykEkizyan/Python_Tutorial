b955b52be4e379f77090dad6161e9b2c7eb51302
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-15 20:12:13 +0400

    comparison methods

--- OOP/Magic_Methods.py
+++ OOP/Magic_Methods.py
@@ -52,6 +52,8 @@ There are equivalent r methods for all magic methods just mentioned.
 
 """
 
+#2
+
 class SpecialString:
     def __init__(self, cont):
         self.cont = cont
@@ -73,4 +75,49 @@ Hello world!
 In the example above, we defined the division operation for our class SpecialString.
 
 """
+
+Python also provides magic methods for comparisons.
+__lt__ for <
+__le__ for <=
+__eq__ for ==
+__ne__ for !=
+__gt__ for >
+__ge__ for >=
+
+If __ne__ is not implemented, it returns the opposite of __eq__.
+There are no other relationships between the other operators.
+
+"""
+
+#3
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+    def __gt__(self, other):
+        for index in range(len(other.cont) + 1):
+            result = other.cont[:index] + ">" + self.cont
+            result += ">" + other.cont[index:]
+            print(result)
+spam = SpecialString("spam")
+eggs = SpecialString("eggs")
+spam > eggs
+
+"""
+>spam>eggs
+e>spam>ggs
+eg>spam>gs
+egg>spam>s
+eggs>spam>
+"""
+
+"""
+
+As you can see, you can define any custom behavior for the overloaded operators.
+
+"""
+
+
+
+
 
