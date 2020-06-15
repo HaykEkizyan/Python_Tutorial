8841d58cba9224299db1e35d33f4a66e7dd625fc
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-15 20:05:16 +0400

    __truediv__ method

--- OOP/Magic_Methods.py
+++ OOP/Magic_Methods.py
@@ -25,5 +25,52 @@ result = first + second
 print(result.x)                                     # 8
 print(result.y)                                     # 16
 
+"""
 
+The __add__ method allows for the definition of a custom behavior for the + operator in our class.
+As you can see, it adds the corresponding attributes of the objects and returns a new object, containing the result.
+Once it's defined, we can add two objects of the class together.
+
+"""
+
+"""
+
+More magic methods for common operators:
+__sub__ for -
+__mul__ for *
+__truediv__ for /
+__floordiv__ for //
+__mod__ for %
+__pow__ for **
+__and__ for &
+__xor__ for ^
+__or__ for |
+
+The expression x + y is translated into x.__add__(y).
+However, if x hasn't implemented __add__, and x and y are of different types, then y.__radd__(x) is called.
+There are equivalent r methods for all magic methods just mentioned.
+
+"""
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+    def __truediv__(self, other):
+        line = "=" * len(other.cont)
+        return "\n".join([self.cont, line, other.cont])
+spam = SpecialString("spam")
+hello = SpecialString("Hello world!")
+print(spam / hello)
+
+"""
+spam
+============
+Hello world!
+"""
+
+"""
+
+In the example above, we defined the division operation for our class SpecialString.
+
+"""
 
