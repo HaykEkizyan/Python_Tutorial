5cdf0e6ce6472af6a2d85aa510183ee1ad26b4c2
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-16 23:31:59 +0400

    cls

---
+++ OOP/Class&Static_Methods.py
@@ -0,0 +1,25 @@
+"""
+
+ Class methods called by a class, which is passed to the cls parameter of the method.
+ A common use of these are factory methods, which instantiate an instance of a class, 
+using different parameters than those usually passed to the class constructor.
+ Class methods are marked with a classmethod decorator.
+ 
+"""
+
+#1 Example
+
+class Rectangle:
+    def __init__(self, widht, height):
+        self.width = widht
+        self.height = height
+    def calculate_area(self):
+        return self.width * self.height
+    @classmethod
+    def new_square(cls,side_length):
+        return cls(side_length, side_length)
+square = Rectangle.new_square(5)
+print(square.calculate_area())                                        # 25
+
+
+
