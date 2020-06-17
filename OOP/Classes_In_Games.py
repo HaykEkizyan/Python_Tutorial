d0b7718dc444a481f64afde3ec4567b1f0e67e91
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-17 16:17:56 +0400

    example

--- OOP/Classes_In_Games.py
+++ OOP/Classes_In_Games.py
@@ -9,6 +9,8 @@
  
 """
 
+#1
+
 def get_input():
     command = input(": ").split()
     verb_word = command[0]
@@ -60,6 +62,8 @@ The next step is to use classes to represent game objects.
 
 """
 
+#2
+
 class GameObject:
     class_name = ""
     desc = ""
@@ -68,6 +72,9 @@ class GameObject:
     def __init__(self, name):
         self.name = name
         GameObject.objects[self.class_name] = self
+       
+    def get_desc(self):
+        return self.class_name + "\n" + self.desc
 
 class Goblin(GameObject):
     class_name = "goblin"
@@ -118,5 +125,79 @@ There is no elf here.
  Combine this code with the one in our previous example, and run the program.
  
  """
-
  
+ """
+ 
+ This code adds more detail to the Goblin class and allows you to fight goblins.
+ 
+ """
+ 
+ #3
+ 
+ class Goblin(GameObject):
+    def __init__(self, name):
+        self.class_name = "goblin"
+        self.health = 3
+        self._desc = " A foul creature"
+        super().__init__(name)
+
+    @property
+    def desc(self):
+        if self.health >=3:
+            return self._desc
+        elif self.health == 2:
+            health_line = "It has a wound on its knee."
+        elif self.health == 1:
+            health_line = "Its left arm has been cut off!"
+        elif self.health <= 0:
+            health_line = "It is dead."
+        return self._desc + "\n" + health_line
+
+    @desc.setter
+    def desc(self, value):
+        self._desc = value
+
+def hit(noun):
+      if noun in GameObject.objects:
+            thing = GameObject.objects[noun]
+            if type(thing) == Goblin:
+                  thing.health = thing.health - 1
+                  if thing.health <= 0:
+                        msg = "You killed the goblin!"
+                  else:
+                        msg = "You hit the {}".format(thing.class_name)
+      else:
+            msg ="There is no {} here.".format(noun)
+      return msg
+     
+"""
+>>>
+: hit goblin
+You hit the goblin
+
+: examine goblin
+goblin
+ A foul creature
+It has a wound on its knee.
+
+: hit goblin
+You hit the goblin
+
+: hit goblin
+You killed the goblin!
+
+: examine goblin
+A goblin
+
+goblin
+ A foul creature
+It is dead.
+:
+"""
+ 
+This was just a simple sample.
+You could create different classes (e.g., elves, orcs, humans), fight them, make them fight each other, and so on.
+
+"""
+
+
