1ae280eb01b1477786b76576f86ebe381332dcdd
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-18 23:05:44 +0400

    Pypi & Pip

---
+++ OOP/Pypi&Pip.py
@@ -0,0 +1,10 @@
+"""
+
+https://pypi.org/
+
+in the terminal put the "pip install openpyxl" command and you can use more libraries. 
+For example "import emoji" command lets you to use more emojies.
+
+Also we can work with spreadsheets...
+
+"""
