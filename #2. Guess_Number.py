c8ee036bff730eccc9ba68df8ab7e1239d7efbd4
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-19 22:54:19 +0400

    #2 secret number is random

--- #2. Guess_Number.py
+++ #2. Guess_Number.py
@@ -1,3 +1,5 @@
+#1 It's a simple example of creating guess game with concret number(9 in this example).
+
 secret_number = 9
 guess_count = 0
 guess_limit = 3
@@ -9,3 +11,15 @@ while guess_count < guess_limit:
         break
 else:
     print('Sorry, you failed!')
+
+    
+    
+#2 It's an example with random number
+
+import random
+
+print("Number guessing game")
+
+# randint function to generate the random number between 1 to 9
+number = random.randint(0, 9)
+
