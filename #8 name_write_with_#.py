1f2a59350a54f1639c0be6238753538f65f53463
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-20 18:09:40 +0400

    all letters to uppercase

--- #8 name_write_with_#.py
+++ #8 name_write_with_#.py
@@ -1,3 +1,4 @@
 name = input("Enter your name: \n")  # in this line you can enter your name
 
-for letter in name:                  # go through all the 26 letters 
+for letter in name:                  # go through all the 26 letters
+        letter = letter.upper()      # all letters will print in uppercase
