84a04b31db16a8cd5014b8fff2a8a3cb8f6f4f20
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-20 17:31:59 +0400

    several examples

--- #7 Extended_Unpacking.py
+++ #7 Extended_Unpacking.py
@@ -16,3 +16,8 @@ def listBeautifier(a):
 print(listBeautifier([3, 4, 2, 4, 38, 4, 5, 3, 2]))        # [4, 38, 4]
 print(listBeautifier([1, 4, -5]))                          # [4]
 print(listBeautifier([]))                                  # []
+print(listBeautifier([8]))                                 # [8]
+print(listBeautifier([10, 2, 10, 9, 7, 3, 8, 5, 3, 2, 8, 7]))                           # []
+print(listBeautifier([8, 5, 1, 2, 3, 8, 1, 10, 5, 1, 4, 6, 9, 2, 8, 8, 9, 4, 10, 3]))   # [8, 1, 10, 5, 1, 4, 6, 9, 2, 8]
+print(listBeautifier([10, 8, 10]))                                                      # [10, 8, 10]
+
