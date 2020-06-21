97c99435d345cb9de4b8f55a11d4ea6f7da8cbc6
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-21 15:10:26 +0400

    for loop to print all rows numbers

--- OOP/Transactions/transactions.py
+++ OOP/Transactions/transactions.py
@@ -4,4 +4,5 @@ sheet = wb['Sheet1']
 cell = sheet['a1']
 cell = sheet.cell(1, 2)
 
-print(cell.value)      # product_id
+for row in range(1, sheet.max_row + 1):
+    print(row)                              # 1 \n 2 \n 3 \n 4
