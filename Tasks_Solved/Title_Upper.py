480b618ac58c50c3a268ece6991d7d0cfa5122a6
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-25 16:08:43 +0400

    Move from another directory

---
+++ Tasks_Solved/Title_Upper.py
@@ -0,0 +1,14 @@
+"""
+
+This task making title uppercase, other symbols lowercase.
+
+"""
+
+
+def fixMessage(message):
+    return message.capitalize()
+
+
+message = fixMessage("mY Hobby is READIng")
+print(message)  # My hobby is reading
+
