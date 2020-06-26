9b2de3a818526595f4b0bd4ded71b39df52ed128
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-26 23:03:36 +0400

    matches for ranges

--- Regular Expressions/Character_Classes.py
+++ Regular Expressions/Character_Classes.py
@@ -6,7 +6,7 @@ A character class is created by putting the characters it matches inside square 
 """
 
 # 1
-
+"""
 import re
 
 pattern = r"[aeiou]"
@@ -19,6 +19,32 @@ if re.search(pattern, "qwertyuiop"):
 
 if re.search(pattern, "rhythm myths"):
     print("Match 3")
-
+"""
 # The pattern [aeiou] in the search function matches all strings that contain any one of the characters defined.
+
+"""
+
+Character classes can also match ranges of characters.
+Some examples:
+The class [a-z] matches any lowercase alphabetic character.
+The class [G-P] matches any uppercase character from G to P.
+The class [0-9] matches any digit.
+Multiple ranges can be included in one class. For example, [A-Za-z] matches a letter of any case.
+
+"""
+
+# 2
+
+import re
+
+pattern = r"[0-9][a-z][a-z]"
+
+if re.search(pattern, "LS8"):
+    print("Match 1")
+
+if re.search(pattern, "E3"):
+    print("match 2")
+
+if re.search(pattern, "1ab"):
+    print("Match 3")                # Match 3
 
