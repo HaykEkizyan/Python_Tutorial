f8770acb2fbfd20549fece6228965fa1d5e2f56f
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-26 14:19:42 +0400

    search(), findall()

--- Regular Expressions/regular_expressions.py
+++ Regular Expressions/regular_expressions.py
@@ -18,7 +18,8 @@ Regular expressions cannot be performed to check whether an email address is rea
 """
 
 Regular expressions in Python can be accessed using the re module, which is part of the standard library.
-After you've defined a regular expression, the re.match function can be used to determine whether it matches at the beginning of a string.
+After you've defined a regular expression, the re.match function can be used to determine whether it matches 
+at the beginning of a string.
 If it does, match returns an object representing the match, if not, it returns None.
 To avoid any confusion while working with regular expressions, we would use raw strings as r"expression".
 Raw strings don't escape anything, which makes use of regular expressions easier.
@@ -39,6 +40,41 @@ else:
 """
 
 The above example checks if the pattern "spam" matches the string and prints "Match" if it does.
-Here the pattern is a simple word, but there are various characters, which would have special meaning when they are used in a regular expression.
+Here the pattern is a simple word, but there are various characters, which would have special meaning 
+when they are used in a regular expression.
+
+"""
+
+"""
+
+Other functions to match patterns are re.search and re.findall.
+The function re.search finds a match of a pattern anywhere in the string.
+The function re.findall returns a list of all substrings that match a pattern.
+
+"""
+
+# 2
+
+import re
+
+pattern = r"spam"
+
+if re.match(pattern, "eggspamsausagespam"):
+    print("Match")
+else:
+    print("No match")                               # No amtch
+
+if re.search(pattern, "eggspamsausagespam"):
+    print("Match")                                  # Match
+else:
+    print("No match")
+
+print(re.findall(pattern, "eggspamsausagespam"))    # ['spam', 'spam']
+
+"""
+
+In the example above, the match function did not match the pattern, as it looks at the beginning of the string.
+The search function found a match in the string.
+The function re.finditer does the same thing as re.findall, except it returns an iterator, rather than a list.
 
 """
