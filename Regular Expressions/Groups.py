c7aaec38562a430dca8b4b1d23af8ef57fa113d8
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-29 23:39:19 +0400

    group(n) & groups()

--- Regular Expressions/Groups.py
+++ Regular Expressions/Groups.py
@@ -21,3 +21,29 @@ if re.match(pattern, "spam"):
     print("Match 3")
 
 # (spam) represents a group in the example pattern shown above.
+
+"""
+
+The content of groups in a match can be accessed using the group function.
+A call of group(0) or group() returns the whole match.
+A call of group(n), where n is greater than 0, returns the nth group from the left.
+The method groups() returns all groups up from 1.
+
+"""
+
+# 2
+
+import re
+
+pattern = r"a(bc)(de)(f(g)h)i"
+
+match = re.match(pattern, "abcdefghijklmnop")
+if match:
+    print(match.group())
+    print(match.group(0))
+    print(match.group(1))
+    print(match.group(2))
+    print(match.groups())
+
+# Groups can be nested.
+
