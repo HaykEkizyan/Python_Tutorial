539a91e1d1f645a84ff3897fcba8017b950273ba
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-29 23:48:59 +0400

    named groups

--- Regular Expressions/Groups.py
+++ Regular Expressions/Groups.py
@@ -53,4 +53,28 @@ x = r"1(23)(4(56)78)9(0)"
 y = re.match(x, "1234567890")
 print(y.group(3))                               # 56
 
+"""
+
+There are several kinds of special groups.
+Two useful ones are named groups and non-capturing groups.
+Named groups have the format (?P<name>...), where name is the name of the group, and ... is the content. 
+They behave exactly the same as normal groups, except they can be accessed by group(name) in addition to its number.
+Non-capturing groups have the format (?:...). 
+They are not accessible by the group method, so they can be added to an existing regular expression 
+without breaking the numbering.
+
+"""
+
+# 3
+
+import re
+
+pattern = r"(?P<first>abc)(?:def)(ghi)"
+
+match = re.match(pattern, "abcdefghi")
+if match:
+    print(match.group("first"))                     # abc
+    print(match.groups())                           # ('abc', 'ghi')
+
+
 
