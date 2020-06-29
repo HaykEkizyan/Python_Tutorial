35ed12bc595b0bd5478aa4fb64dafe2f960dbea9
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-30 00:41:52 +0400

    \d, \s, and \w

--- Regular Expressions/Special_Sequences.py
+++ Regular Expressions/Special_Sequences.py
@@ -7,6 +7,8 @@ This matches the expression of the group of that number.
 
 """
 
+# 1
+
 import re
 
 pattern = r"(.+) \1"
@@ -26,4 +28,36 @@ if match:
 # Note, that "(.+) \1" is not the same as "(.+) (.+)", because \1 refers to the first group's subexpression,
 # which is the matched expression itself, and not the regex pattern.
 
+
+"""
+
+More useful special sequences are \d, \s, and \w.
+These match digits, whitespace, and word characters respectively.
+In ASCII mode they are equivalent to [0-9], [ \t\n\r\f\v], and [a-zA-Z0-9_].
+In Unicode mode they match certain other characters, as well. For instance, \w matches letters with accents.
+Versions of these special sequences with upper case letters - \D, \S, and \W - mean the opposite to the lower-case versions. 
+For instance, \D matches anything that isn't a digit.
+
+"""
+
+# 2
+
+import re
+
+pattern = r"(\D+\d)"
+
+match = re.match(pattern, "Hi 999!")
+
+if match:
+   print("Match 1")                          # Match 1
+
+match = re.match(pattern, "1, 23, 456!")
+if match:
+   print("Match 2")
+
+match = re.match(pattern, " ! $?")
+if match:
+   print("Match 3")
+
+# (\D+\d) matches one or more non-digits followed by a digit.
 
