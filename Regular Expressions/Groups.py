2f8e51597e4c9a6f7468569d484ddd0171cfbda5
Hayk Ekizyan <haykekizyan@gmail.com>
2020-06-29 23:54:55 +0400

    metacharacter |

--- Regular Expressions/Groups.py
+++ Regular Expressions/Groups.py
@@ -76,5 +76,26 @@ if match:
     print(match.group("first"))                     # abc
     print(match.groups())                           # ('abc', 'ghi')
 
+"""
 
+Another important metacharacter is |.
+This means "or", so red|blue matches either "red" or "blue".
+
+"""
+
+import re
+
+pattern = r"gr(a|e)y"
+
+match = re.match(pattern, "gray")
+if match:
+    print("Match 1")                    # Match 1
+
+match = re.match(pattern, "grey")
+if match:
+    print("Match 2")                    # Match 2
+
+match = re.match(pattern, "griy")
+if match:
+    print("Match 3")
 
