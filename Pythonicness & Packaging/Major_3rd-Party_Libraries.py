d91a81e70cd1996901d059376492dace44cd7ea3
Hayk Ekizyan <haykekizyan@gmail.com>
2020-07-02 15:33:36 +0400

    More Libraries in Python

--- Pythonicness & Packaging/Major_3rd-Party_Libraries.py
+++ Pythonicness & Packaging/Major_3rd-Party_Libraries.py
@@ -12,6 +12,18 @@ and leads to better results than building your own scraper with regular expressi
 While Python does offer modules for programmatically accessing websites, such as urllib, they are quite cumbersome to use.
 Third-party library requests make it much easier to use HTTP requests.
 
+
+A number of third-party modules are available that make it much easier to carry out scientific and mathematical computing with Python.
+The module matplotlib allows you to create graphs based on data in Python.
+The module NumPy allows for the use of multidimensional arrays that are much faster than the native Python solution of nested lists.
+It also contains functions to perform mathematical operations such as matrix transformations on the arrays.
+The library SciPy contains numerous extensions to the functionality of NumPy.
+
+Python can also be used for game development.
+Usually, it is used as a scripting language for games written in other languages, but it can be used to make games by itself.
+
+For 3D games, the library Panda3D can be used. For 2D games, you can use pygame.
+
 """
 
 
