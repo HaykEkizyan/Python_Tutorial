308c6a663cd36e575bb33c1aea30cd8b3d8e3741
Hayk Ekizyan <haykekizyan@gmail.com>
2020-07-02 15:31:15 +0400

    Libraries in Python

---
+++ Pythonicness & Packaging/Major_3rd-Party_Libraries.py
@@ -0,0 +1,17 @@
+"""
+
+The Python standard library alone contains extensive functionality.
+However, some tasks require the use of third-party libraries. Some major third-party libraries:
+Django: The most frequently used web framework written in Python, Django powers websites that include Instagram and Disqus.
+It has many useful features, and whatever features it lacks are covered by extension packages.
+CherryPy and Flask are also popular web frameworks.
+
+For scraping data from websites, the library BeautifulSoup is very useful,
+and leads to better results than building your own scraper with regular expressions.
+
+While Python does offer modules for programmatically accessing websites, such as urllib, they are quite cumbersome to use.
+Third-party library requests make it much easier to use HTTP requests.
+
+"""
+
+
