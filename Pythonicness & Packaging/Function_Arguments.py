7c46e1d1ca52a265b931936d69561139f1eab591
Hayk Ekizyan <haykekizyan@gmail.com>
2020-07-02 13:54:30 +0400

    **kwargs

--- Pythonicness & Packaging/Function_Arguments.py
+++ Pythonicness & Packaging/Function_Arguments.py
@@ -36,3 +36,21 @@ function(3, 4, "egg")                   # egg
 # In case the argument is passed in, the default value is ignored.
 # If the argument is not passed in, the default value is used.
 
+"""
+
+**kwargs (standing for keyword arguments) allows you to handle named arguments that you have not defined in advance.
+The keyword arguments return a dictionary in which the keys are the argument names, and the values are the argument values.
+
+"""
+
+# 3
+
+def my_func(x, y=7, *args, **kwargs):
+    print(kwargs)
+
+my_func(2, 3, 4, 5, 6, a=7, b=8)                        # {'a': 7, 'b': 8}
+
+# a and b are the names of the arguments that we passed to the function call.
+# The arguments returned by **kwargs are not included in *args.
+
+
