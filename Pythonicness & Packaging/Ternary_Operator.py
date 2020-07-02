b150cc0f8e17cc69ed869850d99700c4bf63cf4f
Hayk Ekizyan <haykekizyan@gmail.com>
2020-07-02 14:13:19 +0400

    ternary operator / examples

---
+++ Pythonicness & Packaging/Ternary_Operator.py
@@ -0,0 +1,29 @@
+"""
+
+Conditional expressions provide the functionality of if statements while using less code.
+They shouldn't be overused, as they can easily reduce readability, but they are often useful when assigning variables.
+Conditional expressions are also known as applications of the ternary operator.
+
+"""
+
+# 1
+
+a = 7
+b = 1 if a >= 5 else 42
+print(b)                            # 1
+
+"""
+
+The ternary operator checks the condition and returns the corresponding value.
+In the example above, as the condition is true, b is assigned 1. 
+If a was less than 5, it would have been assigned 42.
+
+"""
+
+# 2
+
+status = 1
+msg = "Logout" if status == 1 else "Login"
+print(msg)
+
+# The ternary operator is so called because, unlike most operators, it takes three arguments.
