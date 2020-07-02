b1a8bb5095a17a7df70017328ed9ae068e767582
Hayk Ekizyan <haykekizyan@gmail.com>
2020-07-02 13:48:54 +0400

    default values

--- Pythonicness & Packaging/Function_Arguments.py
+++ Pythonicness & Packaging/Function_Arguments.py
@@ -19,5 +19,20 @@ function(1, 2, 3, 4, 5)                 # 1 \n (2, 3, 4, 5)
 The parameter *args must come after the named parameters to a function.
 The name args is just a convention; you can choose to use another.
 
+
+Named parameters to a function can be made optional by giving them a default value.
+These must come after named parameters without a default value.
+
 """
+
+# 2
+
+def function(x, y, food="spam"):
+    print(food)
+
+function(1, 2)                          # spam
+function(3, 4, "egg")                   # egg
+
+# In case the argument is passed in, the default value is ignored.
+# If the argument is not passed in, the default value is used.
 
