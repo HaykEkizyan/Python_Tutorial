452547182617dc0fb5e4e29bec868f0df7b27efa
Hayk Ekizyan <haykekizyan@gmail.com>
2020-07-02 13:26:33 +0400

    PEP 8 guidelines

---
+++ Pythonicness & Packaging/PEP.py
@@ -0,0 +1,13 @@
+"""
+
+Python Enhancement Proposals(предложения по развитию Python) (PEP)
+are suggestions for improvements to the language, made by experienced Python developers.
+PEP 8 is a style guide on the subject of writing readable code.
+It contains a number of guidelines in reference to variable names, which are summarized here:
+- modules should have short, all-lowercase names;
+- class names should be in the CapWords style;
+- most variables and function names should be lowercase_with_underscores;
+- constants (variables that never change value) should be CAPS_WITH_UNDERSCORES;
+- names that would clash with Python keywords (such as 'class' or 'if') should have a trailing underscore.
+
+"""
